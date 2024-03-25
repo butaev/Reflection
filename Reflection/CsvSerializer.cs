34bc596b69461bbc38b77a59283c8f3e857bdf65
butaev <butaev-raul@mail.ru>
2024-03-26 02:06:34 +0700

    Csv serializer/deserializer

---
+++ Reflection/CsvSerializer.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Reflection;
+
+public static class CsvSerializer
+{
+    public static string Serialize<T>(T obj)
+    {
+        var sb = new StringBuilder();
+        var properties = obj.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        sb.AppendLine(string.Join(",", properties.Select(p => p.Name)));
+        sb.AppendLine(string.Join(",", properties.Select(p => p.GetValue(obj))));
+
+        return sb.ToString();
+    }
+
+    public static T Deserialize<T>(string csvString)
+    {
+        var lines = csvString.Split('\n');
+        var headers = lines[0].Split(',');
+
+        var obj = (T)Activator.CreateInstance(typeof(T));
+
+        var properties = obj.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        for (var i = 1; i < lines.Length; i++)
+        {
+            var values = lines[i].Split(',');
+            for (var j = 0; j < values.Length; j++)
+            {
+                var property = properties.FirstOrDefault(p => p.Name == headers[j]);
+                if (property != null)
+                {
+                    var value = Convert.ChangeType(values[j], property.PropertyType);
+                    property.SetValue(obj, value);
+                }
+            }
+        }
+
+        return obj;
+    }
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+namespace Reflection;
+
+public static class Program
+{
+    private static void Main()
+    {
+        const int iterations = 100000;
+
+        var f1 = F.Get();
+
+        var csvSerializeTime = MeasureTime(() => CsvSerializer.Serialize(f1), iterations);
+        Console.WriteLine("Csv serialize time: {0} мс", csvSerializeTime.totalTime);
+
+        var jsonSerializeTime = MeasureTime(() => System.Text.Json.JsonSerializer.Serialize(f1), iterations);
+        Console.WriteLine("System.Text.Json serialize time: {0} мс", jsonSerializeTime.totalTime);
+        
+        var csvF = File.ReadAllText("f.csv");
+        var csvDeserializeTime = MeasureTime(() => CsvSerializer.Deserialize<F>(csvF), iterations);
+        Console.WriteLine("Csv deserialize time: {0} мс", csvDeserializeTime.totalTime);
+
+        var jsonF = File.ReadAllText("f.json");
+        var jsonDeserializeTime = MeasureTime(() => System.Text.Json.JsonSerializer.Deserialize<F>(jsonF), iterations);
+        Console.WriteLine("System.Text.Json deserialize time: {0} мс", jsonDeserializeTime.totalTime);
+    }
+
+    private static (long totalTime, double averageTime) MeasureTime(Action action, long iterations)
+    {
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+        for (var i = 0; i < iterations; i++)
+        {
+            action();
+        }
+
+        stopwatch.Stop();
+        return (stopwatch.ElapsedMilliseconds, (double)stopwatch.ElapsedMilliseconds / iterations);
+    }
+}
